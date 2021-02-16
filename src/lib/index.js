a0e1a16c7a1509e12ec7ef1219cde7f509311b08
MarinaVegaLeyva <marina.vega.condor@gmail.com>
2021-02-16 16:56:32 -0600

    google authentication

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,37 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
+ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
-};
+}; 
+
+export const authentication = (provider) => {
+  //Para acceder con una ventana emergente, llama a signInWithPopup
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+        console.log(credential);
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        console.log(token);
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+      });
+  };
