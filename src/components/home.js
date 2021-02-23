d6883def7d234c84e6fa751ecfbd9fb12d0671d9
LU <rojaslucia.h@gmail.com>
2021-02-23 12:30:10 -0600

    sign in button fix

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,13 @@
+import { authGoogle } from '../lib/firebase.js';
+
 export const home = `
-<div id="viewSignInScreen">
+<div id="viewLoginScreen">
     <img src="images/logo app2.png" class="logo">
     <h1>INGRESAR</h1>
-    <input type="email" id="emailSignIn" class="input" placeholder="Email">
-    <input type="password" id="passwordSignIn" class="input" placeholder="Contraseña">
-    <input type="button" id="logInSignIn" class="btnLogIn" value="INICIAR SESIÓN">
-    <input type="button" id="signIn" class="btnLogIn" value="CREAR CUENTA">
+    <input type="email" id="emailLogin" class="input" placeholder="Email">
+    <input type="password" id="passwordLogin" class="input" placeholder="Contraseña">
+    <input type="button" id="login" class="btnLogin" value="INICIAR SESIÓN">
+    <input type="button" id="createAccount" class="createAccount" value="CREAR CUENTA">
     <h2>o iniciar sesión con</h2>
     <input type="image" id="gmailIcon" class="gmailIcon" src="images/gmail.png">
     <input type="image" id="facebookIcon" class="facebookIcon" src="images/facebook.png">
